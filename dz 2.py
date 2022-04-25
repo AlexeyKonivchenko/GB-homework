c94234200b7b4cb82563d016cdf0352e94b1a46b
AlexeyKonivchenko <100530583+AlexeyKonivchenko@users.noreply.github.com>
2022-04-25 13:57:15 +0300

    Delete dz 2.py

--- dz 2.py
+++
@@ -1,9 +0,0 @@
-word_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-
-for i, s in enumerate(word_list):
-    if s.isdigit():
-        word_list[i] = f'"{int(s):02d}"'
-    elif s[i:].isdigit():
-        word_list[i] = f'"{s[0]}{int(s[1:]):02d}'
-        print(word_list[i], end=' ')
-print(word_list)
